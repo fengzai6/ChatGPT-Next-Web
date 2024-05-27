b68c2c1840cf9090532665e9e76374fb326f9e17
Nacho.L <nacho.liu@sjfood.com>
2024-05-27 15:04:33 +0800

    flash gemini

--- app/components/popup.tsx
+++ app/components/popup.tsx
@@ -5,7 +5,7 @@ import { IconButton } from "./button";
 import ClearIcon from "../icons/clear.svg";
 import CancelIcon from "../icons/cancel.svg";
 
-export const dateId = 202404262112;
+export const dateId = 202405271503;
 const strDateId = dateId.toString();
 export function PopupComponent(props: { onClose: () => void }) {
   const handleSaveSetting = () => {
@@ -56,6 +56,7 @@ export function PopupComponent(props: { onClose: () => void }) {
           <p className={styles["tags-title"]}>现支持模型如下：</p>
           <p className={styles["tags"]}>
             <span className={styles["tag"]}>gpt-4</span>
+            <span className={styles["tag"]}>gpt-4o</span>
             <span className={styles["tag"]}>gpt-4-1106-preview</span>
             <span className={styles["tag"]}>gpt-4-0125-preview</span>
             <span className={styles["tag"]}>gpt-4-turbo-preview</span>
@@ -64,14 +65,12 @@ export function PopupComponent(props: { onClose: () => void }) {
             {/* <span className={styles["tag"]}>gpt-3.5-turbo-1106</span> */}
             <span className={styles["tag"]}>谷歌gemini-pro</span>
             <span className={styles["tag"]}>谷歌gemini-1.5-pro</span>
+            <span className={styles["tag"]}>谷歌gemini-1.5-flash</span>
             <span className={styles["tag"]}>谷歌gemini-pro-vision</span>
             <span className={styles["tag"]}>claude-3-haiku-20240307</span>
           </p>
           <p className={styles["des"]}>
-            保底可用模型gemini-pro
-            <br />
-            gemini-1.5-pro 每分钟仅支持 2
-            个请求，支持文本和图片一起聊，支持多轮对话
+            保底可用模型gemini-pro系列
           </p>
           <div className={styles["tip"]}>
             <p className={styles["tip-title"]}>PS:</p>
